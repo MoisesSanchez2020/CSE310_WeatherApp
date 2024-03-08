faf935540c86c5571a201ae9a84f9bcde3be2e20
MoisesSanchez2020 <san19039@byui.edu>
2024-03-08 00:38:59 -0800

    update 03/08/2024

--- main.js
+++ main.js
@@ -13,21 +13,30 @@ let weather = {
     displayWeather: function (data) {
       const { name } = data;
       const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
+      const { temp, humidity, feels_like } = data.main;
       const { speed } = data.wind;
-      //console.log(name,icon,description,temp,humidity,speed);
+      const visibility = data.visibility; // Assuming this is in meters
+      const uvIndex = data.uvi; // Assuming UV index is directly available
+    
+      // Convert visibility to kilometers with one decimal place
+      const visibilityInKm = (visibility / 1000).toFixed(1);
+    
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°F"; // Changed the unit to Fahrenheit
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h"; // Improved readability
-  
+      document.querySelector(".temp").innerText = Math.ceil(temp) + "°F"; // Temperature rounded up
+      document.querySelector(".feels-like").innerText = "Feels like: " + Math.ceil(feels_like) + "°F"; // 'Feels like' temperature rounded up
+      document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+      document.querySelector(".visibility").innerText = "Visibility: " + visibilityInKm + " km"; // Added visibility
+      document.querySelector(".uv-index").innerText = "UV Index: " + uvIndex; // Added UV index
+    
       document.querySelector(".weather").classList.remove("loading");
-      // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/? " + name + "')"
-    },
+    }
+    
+    
+    ,
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
     }
